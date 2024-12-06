6e13ffc61c5ec2c377dfa7842dc557f4bba22efc
powdered_bread <104197490+powderedbread@users.noreply.github.com>
2024-12-06 14:55:36 -0700

    Update 001_sphereVolume.cpp

--- 001_sphereVolume.cpp
+++ 001_sphereVolume.cpp
@@ -6,6 +6,6 @@ int main(void)
 	int radius;
 	printf("Enter radius : ");
 	scanf_s("%d", &radius);
-	printf("volume is : %lf \n", (4.0/3.0) * PI * radius * radius * radius);
+	printf("volume is : %f \n", (4.0/3.0) *PI* radius * radius * radius);
 	return 0;
 }
