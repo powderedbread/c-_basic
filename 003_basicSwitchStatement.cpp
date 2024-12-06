b1fe6c24215ee60594e40f4b50d5d6e03e06ecf2
powdered_bread <104197490+powderedbread@users.noreply.github.com>
2024-12-06 16:25:15 -0700

    Update 003_basicSwitchStatement.cpp

--- 003_basicSwitchStatement.cpp
+++ 003_basicSwitchStatement.cpp
@@ -4,10 +4,11 @@
 int main(void) 
 {
     float speed;
-
     printf("Enter speed: ");
-    scanf_s("%f", &speed);  //use %f for float and %lf for double.
-    speed = speed <= 65 ? 65 : speed <= 70 ? 70 : 90;   //if speed <=65, then speed is changed to 65 , else, <=70?, else 90
+    //use %f for float and %lf for double.
+    scanf_s("%f", &speed);  
+    //if speed <=65, then speed is changed to 65 , else, <=70?, else 90
+    speed = speed <= 65 ? 65 : speed <= 70 ? 70 : 90;   
     int speedInt = (int)speed;
 
         switch (speedInt)
