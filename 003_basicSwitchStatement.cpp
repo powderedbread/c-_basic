824f2840515330b3b4f6cb15984d6250faadae8d
powdered_bread <104197490+powderedbread@users.noreply.github.com>
2024-12-06 16:24:19 -0700

    Create 003_basicSwitchStatement.cpp

---
+++ 003_basicSwitchStatement.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>   
+
+//basic switch statement with a weird compound (x) ?  a : b thing.
+int main(void) 
+{
+    float speed;
+
+    printf("Enter speed: ");
+    scanf_s("%f", &speed);  //use %f for float and %lf for double.
+    speed = speed <= 65 ? 65 : speed <= 70 ? 70 : 90;   //if speed <=65, then speed is changed to 65 , else, <=70?, else 90
+    int speedInt = (int)speed;
+
+        switch (speedInt)
+        {
+        case 65: printf("That seems pretty chill"); break;
+        case 70: printf("That's more than 65"); break;
+        case 90: printf("That's a bit too fast"); break;
+
+        }
+    return 0;
+ }  
