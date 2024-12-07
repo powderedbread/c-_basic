592966c951d81759efd8fa458a1b5a84119cffb5
powdered_bread <104197490+powderedbread@users.noreply.github.com>
2024-12-06 21:02:13 -0700

    Update 008_formatAndAppendToVector.cpp

--- 008_formatAndAppendToVector.cpp
+++ 008_formatAndAppendToVector.cpp
@@ -7,33 +7,36 @@
 #include <vector>   // To use vector
 using namespace std;
 
-
 int main() {
-    char theData[] = "5713   6936   4534  8764   3432  3433";  
+    char theData[] = "5713   6936   4534  8764   3432  3433";
 
-    int length = sizeof(theData) ;  
+    int length = sizeof(theData);
     string entry = "";  //used to add chars until a space is reached
     vector<int> theDataFormatted;  // use vector instead an array to use "dynamic" size
+    long int sum= 0;
 
-
-    for (int i = 0; i < length; i++) 
+    for (int i = 0; i < length; i++)
     {
         if (isdigit(theData[i]))                         // Add digit "char" to the current entry text if it is "a digit" 
-        {entry += theData[i];}   
-        
-        else if (!isdigit(theData[i]))   
-        {                          
+        {
+            entry += theData[i];
+        }
+
+        else if (!isdigit(theData[i]))
+        {
             if (entry != "")                              //if entry is not equal to ""
             {
                 theDataFormatted.push_back(stoi(entry));  //convert "entry" to int and append to vector
-                entry = "";                             
+                sum += stoi(entry);
+                entry = "";
             }
         }
 
+    }
     // Print the result (the second number, for example)
     cout << "first number:  " << theDataFormatted[0] << endl;
-    cout << "Second number: " << theDataFormatted[1] << endl;
-    cout << "sixth number: " << theDataFormatted[5] << endl;
-
+    cout << "second number:  " << theDataFormatted[1] << endl;
+    cout << "fifth number:  " << theDataFormatted[4] << endl;
+    cout << "sum is: " << sum;
     return 0;
 }
