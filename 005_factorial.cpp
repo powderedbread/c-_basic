368f58a165bc697aeddc466a33007ea29666aeb7
powdered_bread <104197490+powderedbread@users.noreply.github.com>
2024-12-06 17:50:31 -0700

    Create 005_factorial.cpp

---
+++ 005_factorial.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+ int main(void)
+{
+     int var = 0;
+     int f = 1;
+     printf("Chose number to make factorial: ");
+     scanf_s("%d", &var);
+
+     //start f at 1 not zero, and multiply by +1 until var is reached.
+     for (int i = 1; i <= var; i++)
+        f = f * i;
+  
+    printf("Factorial of %d is %ld\n", var, f);  
+
+    return 0;  
+}
